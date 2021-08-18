1acf656c7ff0b2e8619119fcb344d8d3c4655c42
Pia Paulino <maripia1220@gmail.com>
2021-08-18 14:47:36 -0400

    Created components

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "../styles/Header.css";
+
+
+
+const styles = {
+  headerStyle: {
+    background: "Violet",
+  },
+  headingStyle: {
+    fontSize: "100px",
+  },
+};
+
+
+function Header() {
+  return (
+    <header style={styles.headerStyle} className="header">
+      <h1 style={styles.headingStyle}>Welcome</h1>
+    </header>
+  );
+}
+
+export default Header;
