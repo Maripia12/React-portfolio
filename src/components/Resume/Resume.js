912af2266543db7bc8a2a92b6d42f10796366f81
Pia Paulino <maripia1220@gmail.com>
2021-08-20 13:02:09 -0400

    Updated resume-css

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,17 +1,55 @@
 import React from "react";
+import "./Resume.css";
+
 
 export default function Resume() {
     return (
        
         <div>
 
-        <h1>Download Resume</h1>
+        <h1>Resume</h1>
         
             
             <div> 
-                <a href="">OPEN PDF</a>
+                <a href="https://docs.google.com/document/d/1tkDY9K_rjZuu1TNVI37NaCbOBUBvo_U4/edit?usp=sharing&ouid=110040946983332379633&rtpof=true&sd=true">DOWNLOAD MY RESUME DOCUMENT</a>
 
-               <img src= ""></img> 
+           <h2>
+             Front-End Proficiencies:
+            </h2>
+           <div class = "front" >
+            <ul>
+
+            <li> HTML5</li>
+            <li> CSS </li>
+            <li> Javascript </li>
+            <li> jQuery </li>
+            <li> Responsive Design </li>
+            <li> React </li>
+            <li> Bootstrap </li>
+            </ul>
+            
+            </div>
+
+
+            <h2>
+             Back-End Proficiencies:
+            </h2>
+           <div class = "back" >
+            <ul>
+
+            <li> APIs</li>
+            <li> Node </li>
+            <li> Express </li>
+            <li> MySQL, Sequelize </li>
+            <li> MongoD, Mongoose</li>
+            <li> Rest </li>
+            <li> GraphQL</li>
+            </ul>
+            
+            </div>
+                    
+           
+
 
             </div>
 
@@ -24,3 +62,4 @@ export default function Resume() {
 
     )
 };
+ 
